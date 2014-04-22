d1af622c22e1da52bbe5d2c0ca228adad6aadfa9
Mafia <msx7000@gmail.com>
2014-04-22 10:34:49 +0800

    gradient background

--- Feinsches/ViewController.m
+++ Feinsches/ViewController.m
@@ -13,11 +13,34 @@
 @end
 
 @implementation ViewController
-
+static int i=0;
+static BOOL increase;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    increase=YES;
+    [self gradient];
+}
+
+- (void)gradient {
+    int p;
+    if (increase) {
+        p=i++;
+    }
+    else {
+        p=i--;
+    }
+    
+    if ((increase && i>2000) || (!increase && i<0)) {
+        increase=!increase;
+    }
+    
+    
+    float hey=(float)p/2000;
+    self.view.backgroundColor=[UIColor colorWithRed:hey green:1-hey blue:0 alpha:1];
+//    NSLog(@"%@ %.3f",NSStringFromSelector(_cmd),hey);
+    [self performSelector:_cmd withObject:nil afterDelay:0.01f];
 }
 
 - (void)didReceiveMemoryWarning
