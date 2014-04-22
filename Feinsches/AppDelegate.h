e38fc38010a1d5e60c23acad26beb369887ee2c1
Mafia <msx7000@gmail.com>
2014-04-22 09:30:54 +0800

    Creating the main project

---
+++ Feinsches/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Feinsches
+//
+//  Created by Mafia on 4/22/14.
+//  Copyright (c) 2014 Stephen Sachson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Feinsches/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Feinsches
+//
+//  Created by Mafia on 4/22/14.
+//  Copyright (c) 2014 Stephen Sachson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
